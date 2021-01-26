49c8292753373c6c2ee9b59df9d3f586bfd60b84
Anthony Guinchard <antho.guinchard@gmail.com>
2021-01-26 14:26:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# ipynb2pdf.py
+Python program that automatically converts a ".ipynb" Jupyter Notebook file into
+a readable ".pdf" document.
+
+## Table of contents
+* [1. Description](#1-description)
+* [2. Getting started](#2-getting-started)
+    * [2.1 Dependencies](#21-dependencies)
+    * [2.2 Installing](#22-installing)
+    * [2.3 Executing program](#23-executing-program)
+* [3. Version history](#3-version-history)
+
+<!-- toc -->
+
+## 1. Description
+`ipynb2pdf.py` is a Python program that makes use of the `nbconvert`
+(cf.: https://github.com/jupyter/nbconvert) and `wkhtmltopdf` (cf.:
+https://wkhtmltopdf.org) commands to convert a ".ipynb" file into a ".pdf" file.
+
+
+## 2. Getting started
+
+### 2.1 Dependencies
+* Tested on macOS Big Sur version 11.0.1
+* Python 2.7
+
+### 2.2 Installing
+`pip install -r requirements.txt`
+
+### 2.3 Executing program
+- To access useful help messages, type following Terminal command at the root of
+  the project:
+  
+  `/usr/local/bin/python2.7 ipynb2pdf.py -h`
+
+
+- To convert a single ".ipynb" Jupyter Notebook file into a readable ".pdf"
+  document using the ".ipynb" absolute file path saved in the clipboard, type
+  following Terminal command at the root of the project:
+  
+  `/usr/local/bin/python2.7 ipynb2pdf.py`
+
+  The program will automatically get the clipboard stored ".ipynb" absolute file
+  path and generate the corresponding ".pdf" file version.
+  
+
+- To convert a single ".ipynb" Jupyter Notebook file into a readable ".pdf"
+  document using the ".ipynb" absolute file path provided as a command line
+  argument, type following Terminal command at the root of the project:
+  
+  `/usr/local/bin/python2.7 ipynb2pdf.py --path /path/to/ipynb/file`
+
+  The program will automatically use ".ipynb" absolute file path and generate
+  the corresponding ".pdf" file version.
+
+ 
+Remark: with a Bash Terminal window, `/usr/local/bin/python2.7` can simply be
+replaced by `python`
+
+ 
+## 3. Version history
+* 0.1
+    * Initial release
---
+++ ipynb2pdf.py
@@ -0,0 +1,123 @@
+#!/usr/local/bin/python2.7
+
+
+# ipynb2pdf.py
+
+
+#==================
+debugModeOn = False
+#==================
+
+
+## Setting the current working directory automatically
+import os
+project_path = os.getcwd() # getting the path leading to the current working directory
+os.getcwd() # printing the path leading to the current working directory
+os.chdir(project_path) # setting the current working directory based on the path leading to the current working directory
+
+
+## Required packages
+import os
+import osascript # for displaying the Desktop notification
+from pathlib import Path # for getting the parent directory of the Jupyter Notebook
+from termcolor import colored # (pip install termcolor)
+from argparse import ArgumentParser
+from pandas.io.clipboard import clipboard_get # to access the string situated in the clipboard
+
+
+## Parsing the input argument
+if not debugModeOn:
+    parser = ArgumentParser(description='"ipynb2pdf.py" is a Python program that\
+        automatically converts a ".ipynb" Jupyter Notebook into a readable ".pdf" document.')
+    parser.add_argument('--path', metavar='/path/to/ipynb/file', type=str, default='clipboard', help='convert the ".ipynb" file into a ".pdf" file')
+    args = parser.parse_args()
+    argsVid = args.path
+else: # in case we are in "debug mode"
+    argsVid = '".ipynb" file path required'
+
+
+## Tests (clipboard_value examples)
+# :-) Absolute file path of existing ".ipynb" file:
+#argsVid = '/Users/anthony/MEGA/DOCUMENTS/Programmation/Python/MyPythonProjects/ConvertJupyterNotebookToPDF/tests/classifying_space_rocks.ipynb'
+# :-) Absolute file path of existing ".ipynb" file:
+#argsVid = project_path + '/tests/classifying_space_rocks.ipynb'
+# :-) Absolute file path of nonexistent ".ipynb" file:
+#argsVid = project_path + '/tests/tada.ipynb'
+
+
+## Function
+
+def notify(message, title, subtitle, sound):
+    """
+    Posts macOS X notification
+
+    Args:
+        message (str): The message
+        title (str): The title
+        subtitle (str): The subtitle
+        sound (str): The macOS X sound
+    """
+
+    code, out, err = osascript.run('display notification "{0}" with title "{1}" subtitle "{2}" sound name "{3}"'.format(message, title, subtitle, sound))
+
+
+## Main process
+
+if argsVid == 'clipboard': # this is True (i.e. argsVid == 'clipboard') only if debugModeOn is set to "False"
+    # 1) Retrieving stored clipboard value
+    print('\n1) Retrieving stored clipboard value')
+    clipboard_value = clipboard_get()
+else:
+    # 1) Retrieving the ".ipynb" file path argument
+    print('\n1) Retrieving the ".ipynb" file path argument')
+    clipboard_value = argsVid
+print(' clipboard_value: {0}'.format(clipboard_value.encode('utf-8')))
+
+if os.path.isfile(clipboard_value): # checking if the file exists on the computer (cf.: https://linuxize.com/post/python-check-if-file-exists/)
+
+    try:
+
+        # 2) Converting ".ipynb" to ".html" (cf.: https://github.com/jupyter/nbconvert)
+        # (Example: jupyter nbconvert --to html my_file.ipynb)
+        print('2) Converting ".ipynb" to ".html"')
+        ipynb_file_path = clipboard_value
+        os.system('jupyter nbconvert --to html {0}'.format(ipynb_file_path))
+
+        # 3) Converting ".html" to ".pdf" (cf.: https://wkhtmltopdf.org)
+        # (Example: wkhtmltopdf my_file.html my_file.pdf)
+        print('3) Converting ".html" to ".pdf"')
+        html_file_path = ipynb_file_path.replace('.ipynb', '.html')
+        pdf_file_path = ipynb_file_path.replace('.ipynb', '.pdf')
+        os.system('wkhtmltopdf {0} {1}'.format(html_file_path, pdf_file_path))
+
+        # 4) Removing the useless ".html" file
+        print('4) Removing the useless ".html" file')
+        os.remove(html_file_path)
+
+        # 5) Posting macOS X notification
+        print('5) Posting macOS X notification\n')
+        parent_directory = Path(pdf_file_path).parent
+        notify(title='ipynb2pdf.py',
+               subtitle='Jupyter Notebook converted into PDF :-)',
+               message='The PDF file is available in {0}'.format(parent_directory),
+               sound='Hero')
+
+    except Exception as e:
+        colored_error_message = colored('A problem occurred while trying to convert ".ipynb" file into ".pdf" file...', 'red', attrs=['reverse', 'blink'])
+        print(':-/ ERROR! ' + colored_error_message + '\n Error message:\n  {0}'.format(e))
+
+        # Posting macOS X notification
+        notify(title='ipynb2pdf.py',
+               subtitle='ERROR: Jupyter Notebook NOT converted into PDF :-(',
+               message='A problem occurred in the conversion of the Jupyter Notebook into PDF...',
+               sound='Sosumi')
+
+else:
+    colored_error_message = colored('The ".ipynb" file path does not exist...', 'red', attrs=['reverse', 'blink'])
+    print(' :-/ ERROR! ' + colored_error_message)
+
+    # Posting macOS X notification
+    notify(title='ipynb2pdf.py',
+           subtitle='ERROR: Jupyter Notebook NOT converted into PDF :-(',
+           message='The Jupyter Notebook file path does not exist...',
+           sound='Sosumi')
